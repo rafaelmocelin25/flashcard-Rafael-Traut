cc84e055c83825914fe00e895c19973bf343c903
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2024-10-22 09:16:19 -0300

    dsdd

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,30 @@
-
+Criacartao(
+    'Jogos',
+    'Qual o jogo com mais copias vendidas?',
+    'Minecraft',
+)
+Criacartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls',
+)
+Criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+Criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+Criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+Criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
