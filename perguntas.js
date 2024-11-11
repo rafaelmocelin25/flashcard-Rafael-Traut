cccf147d7f7ab3b74fbb2a473ec1d94ddbf64e25
rafaelmocelin25 <141454436+rafaelmocelin25@users.noreply.github.com>
2024-11-11 10:52:55 -0300

    dsd

--- perguntas.js
+++ perguntas.js
@@ -8,3 +8,23 @@ criaCartao(
     'Quais jogos são conhecidos por serem dificeis e sem pause?',
     'A serie souls'
 )
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
+criaCartao(
+    'Jogos',
+    'Quais jogos são conhecidos por serem dificeis e sem pause?',
+    'A serie souls'
+)
